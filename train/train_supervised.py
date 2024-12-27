6ace3384f7e5e8ed900c0f3b8dd2d590fba2ec4d
Greyson Brothers <greysonbrothers@gmail.com>
2024-12-26 20:36:27 -0500

    Update supervised training script to use k-fold cross validation (5-fold by default)

--- train/train_supervised.py
+++ train/train_supervised.py
@@ -10,6 +10,7 @@ from tqdm import tqdm
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.utils.data.dataset import Subset
 from torch.utils.data import DataLoader, TensorDataset, random_split
 torch.set_float32_matmul_precision('high')
 
@@ -61,7 +62,7 @@ def configure_logger(path, config):
     ## SAVE INITIAL LOG
     history_path = os.path.join(log_dir, f"history.csv") 
     log(history_path, "Train Loss", "Val Loss", "Wall Time")
-    return log_dir, history_path
+    return log_dir
 
 
 def log(path, train_loss, val_loss, wall_time):
@@ -88,17 +89,17 @@ def load_checkpoint(model, path):
     device = next(model.parameters()).device
     state_dict = torch.load(checkpoint, weights_only=True, map_location=device)
 
-    unwanted_prefix = '_orig_mod.'
-    for key in list(state_dict.keys()):
-        if key.startswith(unwanted_prefix):
-            state_dict[key[len(unwanted_prefix):]] = state_dict.pop(key)
+    # unwanted_prefix = '_orig_mod.'
+    # for key in list(state_dict.keys()):
+    #     if key.startswith(unwanted_prefix):
+    #         state_dict[key[len(unwanted_prefix):]] = state_dict.pop(key)
     model.load_state_dict(state_dict)
     model.eval()
     return model
     
 
 
-def load_dataset(experiment_path, dim_vectors, seed):
+def load_dataset(experiment_path, dim_vectors):
     # Load data
     method = os.path.basename(experiment_path)
     data_path = os.path.dirname(experiment_path).replace("experiments", "data")
@@ -110,23 +111,23 @@ def load_dataset(experiment_path, dim_vectors, seed):
     y = torch.tensor(y, dtype=torch.float32)
 
     # Create a full dataset
-    full_dataset = TensorDataset(X, y)
+    return TensorDataset(X, y)
 
-    # Split into train, validation, and test datasets
-    train_ratio, val_ratio, test_ratio = 0.7, 0.15, 0.15
-    total_size = len(full_dataset)
-    train_size = int(train_ratio * total_size)
-    val_size = int(val_ratio * total_size)
-    test_size = total_size - train_size - val_size  # Ensure total size matches
+    # # Split into train, validation, and test datasets
+    # train_ratio, val_ratio, test_ratio = 0.7, 0.15, 0.15
+    # total_size = len(full_dataset)
+    # train_size = int(train_ratio * total_size)
+    # val_size = int(val_ratio * total_size)
+    # test_size = total_size - train_size - val_size  # Ensure total size matches
 
-    train_dataset, val_dataset, test_dataset = random_split(
-        full_dataset, [train_size, val_size, test_size], 
-        generator=torch.Generator().manual_seed(seed)  # Ensures deterministic split
-    )
-    return train_dataset, val_dataset, test_dataset
+    # train_dataset, val_dataset, test_dataset = random_split(
+    #     full_dataset, [train_size, val_size, test_size], 
+    #     generator=torch.Generator().manual_seed(seed)  # Ensures deterministic split
+    # )
+    # return train_dataset, val_dataset, test_dataset
 
 
-def train(
+def kfold(
         experiment_path:str,
         config:dict,
         load_dataset,
@@ -134,43 +135,93 @@ def train(
     ## PARSE CONFIG
     model_config = config["MODEL_CONFIG"]
     dim_vectors = config["LEARNING_PARAMETERS"]["DIM_INPUT"]
-    epochs = config["LEARNING_PARAMETERS"]["EPOCHS"]
-    debug = config["LEARNING_PARAMETERS"]["DEBUG"]
+    test_ratio = config["LEARNING_PARAMETERS"]["TEST_RATIO"]
     bs = config["LEARNING_PARAMETERS"]["BATCH_SIZE"]
-    lr = config["LEARNING_PARAMETERS"]["LEARNING_RATE"]
-
-    ## CONFIGURE LOGGING AND SEED
-    log_dir, history_path = configure_logger(experiment_path, config)
-    seed = config["LEARNING_PARAMETERS"]["SEED"]
+    k = config["LEARNING_PARAMETERS"]["NUM_FOLDS"]
+    debug = config["LEARNING_PARAMETERS"]["DEBUG"]
+    results = []
 
     ## BUILD DATASETS
-    train_dataset, val_dataset, test_dataset = load_dataset(experiment_path, dim_vectors, seed)
-    train_loader = DataLoader(train_dataset, batch_size=bs, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=bs, shuffle=False)
-    test_loader = DataLoader(test_dataset, batch_size=bs, shuffle=False)
+    dataset = load_dataset(experiment_path, dim_vectors)
+
+    ## KFOLD VARIABLES
+    data_shape = dataset.tensors[0].shape
+    total_size = data_shape[0]
+    test_size = int(total_size * test_ratio)
+    train_size = total_size - test_size
+    fold_size = train_size // k
+    test_idxs = list(range(train_size, total_size))
 
     ## INITIALIZE MODEL
-    X_shape = train_dataset.dataset.tensors[0].shape
-    y_shape = train_dataset.dataset.tensors[1].shape
     model_config.update({
-        "num_vectors": X_shape[1],
+        "num_vectors": data_shape[1],
         "dim_ff": 4*model_config["dim_hidden"],
-        # "dim_hidden": X_shape[2],
+        # "dim_hidden": data_shape[2],
     })
     model_class = MODELS[model_config["model"]]
-    model = model_class(**model_config)
-    
-    ## SET DEVICE 
-    initial_gpu_mem = get_gpu_memory()
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model.to(device)
 
-    ## COMPILE MODEL
-    if not debug:
-        model = torch.compile(model) # requires PyTorch 2.0
-    parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print(f"{parameters:,} LEARNABLE PARAMETERS -> {convert_size(get_gpu_memory()-initial_gpu_mem)}")        
+    ## TRAIN ON EACH FOLD
+    for i in range(k):
+        
+        ## CONFIGURE LOGGING AND SEED
+        log_dir = configure_logger(experiment_path, config)
+        config["LEARNING_PARAMETERS"]["LOG_DIR"] = log_dir
+        seed = config["LEARNING_PARAMETERS"]["SEED"]
+
+        ## INITIALIZE NEW MODEL
+        model = model_class(**model_config)
+        
+        ## SET DEVICE 
+        initial_gpu_mem = get_gpu_memory()
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        model.to(device)
+
+        ## COMPILE MODEL
+        if not debug:
+            model = torch.compile(model) # requires PyTorch 2.0
+        parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
+        print(f"{parameters:,} LEARNABLE PARAMETERS -> {convert_size(get_gpu_memory()-initial_gpu_mem)}")        
+        
+        ## LOAD SPLIT
+        vL = i * fold_size
+        vR = vL + fold_size
+        train_idxs_left = list(range(0, vL)) if vL>0 else []
+        train_idxs_right = list(range(vR, train_size)) if vR<train_size else []
+        train_idxs = train_idxs_left + train_idxs_right
+        val_idxs   = list(range(vL, vR))
+        train_loader = DataLoader(Subset(dataset, train_idxs), batch_size=bs, shuffle=True,  generator=torch.Generator().manual_seed(seed))
+        val_loader   = DataLoader(Subset(dataset, val_idxs),   batch_size=bs, shuffle=False, generator=torch.Generator().manual_seed(seed))
+        test_loader  = DataLoader(Subset(dataset, test_idxs),  batch_size=bs, shuffle=False, generator=torch.Generator().manual_seed(seed))
+
+        print(f"\nTRAINING FOLD {i+1}/{k}")
+        test_loss = train(
+            train_loader, 
+            val_loader, 
+            test_loader,
+            model,
+            config,
+        )
+        results.append(test_loss)
     
+    print(f"{k}-FOLD TEST RESULTS: {np.mean(results):.5f} ±{np.std(results):.5f}")
+    return
+
+
+def train(
+        train_loader: DataLoader, 
+        val_loader: DataLoader, 
+        test_loader: DataLoader,
+        model: nn.Module,
+        config: dict,
+    ):
+    ## PARSE CONFIG
+    lr = config["LEARNING_PARAMETERS"]["LEARNING_RATE"]
+    seed = config["LEARNING_PARAMETERS"]["SEED"]
+    epochs = config["LEARNING_PARAMETERS"]["EPOCHS"]
+    log_dir = config["LEARNING_PARAMETERS"]["LOG_DIR"]
+    history_path = os.path.join(log_dir, f"history.csv") 
+    device = next(model.parameters()).device
+
     ## INITIALIZE LOSS AND OPTIMIZER
     criterion = nn.MSELoss() 
     optimizer = optim.Adam(model.parameters(), lr=lr)
@@ -240,7 +291,7 @@ def train(
     log(history_path, "TEST LOSS", test_loss, 0.0)
     print(f"\nEXPERIMENT COMPLETE | Best Val Loss: {best_loss:.4f} | Test Loss: {test_loss:.4f}\n")
     save_checkpoint(log_dir, model, test_loss, tag="final_")
-    return
+    return test_loss
 
 
 
@@ -249,39 +300,48 @@ if __name__ == "__main__":
     # PARSE ARGUMENTS
     parser = argparse.ArgumentParser(description=None)
     parser.add_argument('-p', '--experiment_path', default="./experiments/knn1", help='Relative path to the experiment config.')
+    parser.add_argument('-l', '--loop', default=True, help='Loop through path list.')
     args = parser.parse_args()
 
-    # paths = [
-    #     "./experiments/max",
-    #     "./experiments/min",
-    #     "./experiments/mean",
-    #     "./experiments/subset",
-    #     "./experiments/combo",
-    # ]
-    # base = __file__.split("train")[0]
-    # configs = []
-    # for path in paths:
-    #     with open(os.path.join(base, path, "config.yml"), "r") as file:
-    #         configs.append(yaml.safe_load(file))
 
-    # for path,config in list(zip(paths, configs)):
-    #     ## TRAIN
-    #     method = config["MODEL_CONFIG"].get("pooling_method", "mlp").upper()
-    #     target = os.path.basename(path).upper()
-    #     for i in range(config["LEARNING_PARAMETERS"]["NUM_EXPERIMENTS"]):
-    #         print(f"\nSTARTING EXPERIMENT FOR {method} POOLING APPROXIMATION OF {target}:  {i+1}/{config['LEARNING_PARAMETERS']['NUM_EXPERIMENTS']}")
-    #         train(path, config, load_dataset)
-
-
-    ## LOAD CONFIG
+    paths = [
+        "./experiments/min",
+        "./experiments/max",
+        "./experiments/mean",
+        "./experiments/knn1",
+        "./experiments/knn2",
+        "./experiments/knn4",
+        "./experiments/knn8",
+        "./experiments/knn16",
+        # "./experiments/subset",
+        # "./experiments/combo",
+    ]
     base = __file__.split("train")[0]
-    path = os.path.join(base, args.experiment_path)
-    with open(os.path.join(path, "config.yml"), "r") as file:
-        config = yaml.safe_load(file)
+    configs = []
+    for path in paths:
+        with open(os.path.join(base, path, "config.yml"), "r") as file:
+            configs.append(yaml.safe_load(file))
 
-    ## TRAIN
-    method = config["MODEL_CONFIG"].get("pooling_method", "mlp") #.upper()
-    target = os.path.basename(args.experiment_path).upper()
-    for i in range(config["LEARNING_PARAMETERS"]["NUM_EXPERIMENTS"]):
-        print(f"\nSTARTING EXPERIMENT FOR {method} APPROXIMATION OF {target}:  {i+1}/{config['LEARNING_PARAMETERS']['NUM_EXPERIMENTS']}")
-        train(path, config, load_dataset)
+    for path,config in list(zip(paths, configs)):
+        ## TRAIN
+        method = config["MODEL_CONFIG"].get("pooling_method", "mlp") #.upper()
+        target = os.path.basename(path).upper()
+        path = os.path.join(base, path)
+        for i in range(config["LEARNING_PARAMETERS"]["NUM_EXPERIMENTS"]):
+            print(f"\nSTARTING EXPERIMENT FOR {method} POOLING APPROXIMATION OF {target}:  {i+1}/{config['LEARNING_PARAMETERS']['NUM_EXPERIMENTS']}")
+            kfold(path, config, load_dataset)
+
+
+    # ## LOAD CONFIG
+    # base = __file__.split("train")[0]
+    # path = os.path.join(base, args.experiment_path)
+    # with open(os.path.join(path, "config.yml"), "r") as file:
+    #     config = yaml.safe_load(file)
+
+    # ## TRAIN
+    # method = config["MODEL_CONFIG"].get("pooling_method", "mlp") #.upper()
+    # target = os.path.basename(args.experiment_path).upper()
+    # for i in range(config["LEARNING_PARAMETERS"]["NUM_EXPERIMENTS"]):
+    #     print(f"\nSTARTING EXPERIMENT FOR {method} APPROXIMATION OF {target}:  {i+1}/{config['LEARNING_PARAMETERS']['NUM_EXPERIMENTS']}")
+    #     kfold(path, config, load_dataset)
+    #     # train(path, config, load_dataset)
