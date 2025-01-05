2dfeb795e95ccec8ed98281988123eb2b0cee1a4
brothag1 <greyson.brothers@jhuapl.edu>
2025-01-05 11:50:43 -0500

    Update autoencoder training script to use kfold

--- train/train_autoencoder.py
+++ train/train_autoencoder.py
@@ -1,19 +1,13 @@
 import os
-import time
 import yaml
-import glob
-import random
 import argparse
 import numpy as np
-from tqdm import tqdm
 
 import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader, TensorDataset, random_split
+from torch.utils.data import TensorDataset
 torch.set_float32_matmul_precision('high')
 
-from .train_supervised import kfold
+from train.train_supervised import kfold
 from pooling.models.attenuator import Attenuator
 from pooling.models.autoencoder import Autoencoder
 from pooling.models.aggregation_mlp import AggregationMLP
@@ -33,46 +27,31 @@ class AutoencoderDataset(TensorDataset):
         return len(self.data)
 
     def __getitem__(self, idx):
-        x = self.data[idx]
-        return x, x  # Input and target are the same
+        X = self.data[idx]
+        return X, X  # Input and target are the same
 
     @property
     def tensors(self):
         return (self.data, self.data)
 
 
-def load_dataset(experiment_path, seed):
+def load_dataset(experiment_path, dim_vectors):
     # Load data
-    method = os.path.basename(experiment_path)
-    data_path = os.path.dirname(experiment_path)
-    X = np.load(os.path.join(data_path, f"X.npy"))   # SHAPE: [batch, num_vectors, dim_vectors]
+    data_path = os.path.dirname(experiment_path).replace("experiments", "data")
+    X = np.load(os.path.join(data_path, f"X{dim_vectors}.npy"))   # SHAPE: [batch, num_vectors, dim_vectors]
 
     # Convert to torch tensors
     X = torch.tensor(X, dtype=torch.float32)
 
     # Create a full dataset
-    full_dataset = AutoencoderDataset(X)
-
-    # Split into train, validation, and test datasets
-    train_ratio, val_ratio, test_ratio = 0.7, 0.15, 0.15
-    total_size = len(full_dataset)
-    train_size = int(train_ratio * total_size)
-    val_size = int(val_ratio * total_size)
-    test_size = total_size - train_size - val_size  # Ensure total size matches
-
-    train_dataset, val_dataset, test_dataset = random_split(
-        full_dataset, [train_size, val_size, test_size], 
-        generator=torch.Generator().manual_seed(seed)  # Ensures deterministic split
-    )
-    return train_dataset, val_dataset, test_dataset
-
+    return AutoencoderDataset(X)
 
 
 if __name__ == "__main__":
 
     ## PARSE ARGUMENTS
     parser = argparse.ArgumentParser(description=None)
-    parser.add_argument('-p', '--experiment_path', default="./datasets/autoencoder", help='Path to the dataset.')
+    parser.add_argument('-p', '--experiment_path', default="./experiments/autoencoder", help='Path to the dataset.')
     args = parser.parse_args()
 
     # paths = [
@@ -115,4 +94,3 @@ if __name__ == "__main__":
     for i in range(config["LEARNING_PARAMETERS"]["NUM_EXPERIMENTS"]):
         print(f"\nSTARTING EXPERIMENT FOR {method} APPROXIMATION OF {target}:  {i+1}/{config['LEARNING_PARAMETERS']['NUM_EXPERIMENTS']}")
         kfold(path, config, load_dataset)
-        # train(args.experiment_path, config, load_dataset)
