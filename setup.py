5451847eb134715c9559b17a2e24743a07433fef
Greyson Brothers <greysonbrothers@gmail.com>
2025-01-30 22:07:21 -0500

    Remove any personal identifiers for anonmyous code submission

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import find_packages, setup
 
 setup(
     name="pooling",
-    version="0.0.0",
-    author="Greyson Brothers",
-    author_email="greysonbrothers@gmail.com",
-    description="Code repository for reproducing results from the `Attenuation Networks` paper",
-    long_description="Code repository for reproducing results from the `Attenuation Networks` paper",
+    version="1.0.0",
+    author="anonymous",
+    author_email="anonymous",
+    description="Code repository for reproducing results from the `Robust Noise Attenuation via Adaptive Pooling of Transformer Outputs` paper",
+    long_description="Code repository for reproducing results from the `Robust Noise Attenuation via Adaptive Pooling of Transformer Outputs` paper",
     long_description_content_type="text/markdown",
     url="",
-    packages=[package for package in find_packages() if package.startswith("attenuator")],
+    packages=[package for package in find_packages() if package.startswith("pooling")],
     zip_safe=False,
     install_requires=[
         "torch",
