Fix attention masking implementation

pooling/nn/attention.py
@@ -33,25 +33,19 @@ class MultiHeadAttention(nn.Module):
 
         ## COMPUTE ATTENTION
         if self.flash:
-            attn_mask = torch.tile(mask, (q.size(-2), 1)) if mask is not None else None 
-            e = F.scaled_dot_product_attention(q, k, v, attn_mask=attn_mask, dropout_p=self.dropout_w.p if self.training else 0, is_causal=False)
+            e = F.scaled_dot_product_attention(
+                q, k, v, 
+                attn_mask=mask, 
+                dropout_p=self.dropout_w.p if self.training else 0, 
+                is_causal=False,
+                scale=self.scale,
+            )
             e = e.transpose(1, 2).contiguous().view_as(query) 
         else:
             dot_product = torch.einsum("bhqa,bhka->bhqk", (q, k))
-            dot_product = self.apply_mask(dot_product * self.scale, mask)
+            dot_product = self.scale * dot_product.masked_fill_(mask.logical_not(), float("-inf"))
             w = torch.softmax(dot_product, dim=-1)
             w = self.dropout_w(w)
-            e = torch.einsum("bhqv,bhva->bhqa", (w, v)).contiguous().view_as(query) 
-        
-        return self.dropout_e(self.out(e))
+            e = torch.einsum("bhqv,bhva->bhqa", (w, v)).transpose(1, 2).contiguous().view_as(query) 
 
-    def apply_mask(self, x, mask):
-        ## TRUE VALUES GET MASKED
-        if mask is None:
-            return x
-        if len(mask.shape) == 1: 
-            mask = mask.unsqueeze(0)
-        # if len(mask.shape) == 2: 
-        #     mask = mask.unsqueeze(2)
-        return x + mask #.masked_fill(mask, float('-inf'))
-    
+        return self.dropout_e(self.out(e))

pooling/wrappers/actor_critic.py
@@ -304,8 +304,9 @@ class TokenActorCritic(nn.Module):
         ## RETURN INPUT MASK
         if self._idx_mask is None or mask is not None:
             return None
-        ## RETREIVE MASK FROM TOKENS
-        return tokens[..., self._idx_mask]
+        mask = tokens[..., self._idx_mask].to(bool).logical_not()
+        mask = mask.unsqueeze(1).expand((-1, mask.size(-1), -1))
+        return mask.unsqueeze(1)
 
     def get_value(self):
         return self.value